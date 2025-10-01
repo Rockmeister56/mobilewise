0b396732d5f9a0d02393cf955e84b2423700316c
Rockmeister56 <megacake.records@gmail.com>
2025-10-01 03:58:32 -0700

    Update voice-chat-fusion.js

--- voice-chat-fusion.js
+++ voice-chat-fusion.js
@@ -1247,10 +1247,10 @@ freeBookWithConsultation: {
         
         @keyframes redPulseGlow {
             0%, 100% { 
-                box-shadow: 0 10px 10px rgba(0,0,7,0.0), 0 0 10px rgba(0, 195, 255, 8);
+                box-shadow: 0 10px 10px rgba(0,0,7,0.0), 0 0 10px rgba(0, 255, 0, 1);
             }
             50% { 
-                box-shadow: 0 20px 10px rgba(0,0,73,0.0), 0 0 25px rgba(0, 132, 255, 8);
+                box-shadow: 0 20px 10px rgba(0,0,7,0.0), 0 0 25px rgba(0, 217, 255, 1);
             }
         }
         
