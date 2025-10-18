4e1de223e550150f0f2a3b3f8d753718c5ee358a
Rockmeister56 <megacake.records@gmail.com>
2025-10-17 17:13:57 -0700

    Update voice-chat-fusion-CLEANED.js

--- voice-chat-fusion-CLEANED.js
+++ voice-chat-fusion-CLEANED.js
@@ -4375,7 +4375,7 @@ if (isMobile) {
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        margin-top: 23px;
+        margin-top: 18px;
     `;
 }
     
