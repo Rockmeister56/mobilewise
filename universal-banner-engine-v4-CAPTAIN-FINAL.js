004ebbdc62a89c75bbe747f43b611169f34fd555
Rockmeister56 <megacake.records@gmail.com>
2025-10-25 00:10:29 -0700

    Update universal-banner-engine-v4-CAPTAIN-FINAL.js

--- universal-banner-engine-v4-CAPTAIN-FINAL.js
+++ universal-banner-engine-v4-CAPTAIN-FINAL.js
@@ -79,7 +79,7 @@
     <!-- LEFT: 5-STAR ICON (Stationary, no glow) -->
     <div style="flex-shrink: 0;">
         <img src="https://odetjszursuaxpapfwcy.supabase.co/storage/v1/object/public/form-assets/logos/logo_5f42f026-051a-42c7-833d-375fcac74252_1761252676241_5starpng.png" 
-             style="width: 130px; height: 95px; border-radius: 0px;">
+             style="width: 130px; height: 95px; border-radius: 0px; margin-right: -10px;">
     </div>
     
     <!-- CENTER/RIGHT: Reviews icon + Text -->
@@ -87,7 +87,7 @@
         <!-- REVIEWS ICON (keeps glow) -->
         <img src="https://odetjszursuaxpapfwcy.supabase.co/storage/v1/object/public/form-assets/logos/logo_5f42f026-051a-42c7-833d-375fcac74252_1761252832996_reviews.png" 
              class="book-white-glow"
-             style="width: 70px; height: 70px; border-radius: 0px; margin-right: 40px;">
+             style="width: 70px; height: 70px; border-radius: 0px; margin-right: 60px;">
         
         <!-- TEXT -->
         <div style="color: white; text-align: left;">
