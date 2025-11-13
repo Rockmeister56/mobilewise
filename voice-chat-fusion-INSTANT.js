59fd64607ff963d13182a9acc44c34b317eb8134
Rockmeister56 <megacake.records@gmail.com>
2025-11-12 19:38:58 -0800

    Update voice-chat-fusion-INSTANT.js

--- voice-chat-fusion-INSTANT.js
+++ voice-chat-fusion-INSTANT.js
@@ -4493,7 +4493,7 @@ async function showDirectSpeakNow() {
     }
             
             .speak-now-state {
-                background: rgba(15, 23, 97, 1) !important;
+                background: rgba(19, 36, 192, 1) !important;
                 border-color: rgba(34, 197, 94, 0.8) !important;
                 color: #ffffff !important;
                 animation: speak-now-pulse 2s infinite;
