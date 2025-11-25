5ea7d3dc766b70bfb522ea76a9a4972bb7df2a32
Rockmeister56 <megacake.records@gmail.com>
2025-11-25 03:23:41 -0800

    Update universal-banner-engine-v4-CAPTAIN-FINAL.js

--- universal-banner-engine-v4-CAPTAIN-FINAL.js
+++ universal-banner-engine-v4-CAPTAIN-FINAL.js
@@ -453,7 +453,7 @@ const BANNER_STYLES = `
 }
 
 .branding-banner img {
-    width: 100px !important; /* Larger logo size for better visibility */
+    width: 50px !important; /* Larger logo size for better visibility */
     height: auto !important;
     margin-right: 0 !important;
 }
